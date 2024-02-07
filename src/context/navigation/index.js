f1fd2b25b4dc881a6c79b8c729f12d4c30c62523
loginn <loginn>
2024-02-07 18:04:16 +0300

    done

---
+++ src/context/navigation/index.js
@@ -0,0 +1,7 @@
+const ROUTES = {
+  AUTH_NAVIGATION: {
+    LOGIN: "Login Page",
+    SIGNUP: "signup",
+  },
+};
+export default ROUTES;
---
+++ src/screens/Signup.js
@@ -0,0 +1,34 @@
+import { View, Text, TextInput, Button, Pressable } from "react-native";
+import React from "react";
+import { useNavigation } from "@react-navigation/native";
+import ROUTES from "../context/navigation";
+
+const Signup = () => {
+  const navigation = useNavigation();
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Username</Text>
+      <TextInput placeholder="please enter your username:" />
+      <Text>Password</Text>
+      <TextInput placeholder="please enter your password:" />
+      <Button title="Submit" />
+      <View>
+        <Pressable
+          onPress={() => {
+            navigation.navigate(ROUTES.AUTH_NAVIGATION.LOGIN);
+          }}
+        >
+          <Text>Dont have an account? Signup</Text>
+        </Pressable>
+      </View>
+    </View>
+  );
+};
+
+export default Signup;
